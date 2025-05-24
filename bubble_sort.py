41d525ee045a2d8a4062526f44e1e92bf318f7fa
Max <max@max>
2025-05-24 12:55:41 +0500

    добавил bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+a = [6, 4, 8, 4, 1, 5, 7, 10, -5, -7, -3]
+
+N = len(a)
+for i in range(N-1):
+    for i in range(N-1):
+        if a[i] > a[i+1]:
+            a[i], a[i+1] = a[i+1], a[i]
+
+    print(a)
