076243a1e4622aaf069020f0cc868b422912586f
Max <max@max>
2025-05-24 13:22:29 +0500

    обернул в функцию

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,11 @@
 a = [6, 4, 8, 4, 1, 5, 7, 10, -5, -7, -3]
 
 N = len(a)
-for i in range(N-1):
-    for i in range(N-1):
-        if a[i] > a[i+1]:
-            a[i], a[i+1] = a[i+1], a[i]
 
-    print(a)
+def bubble_sort(a):
+    for i in range(N-1):
+        for i in range(N-1):
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+
+        print(a)
